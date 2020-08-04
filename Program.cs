0a55a9bb94e772044e400c03578b03a7df1d7547
artemya <artemya@tahometer.com>
2020-08-04 18:34:00 +0300

    Add color to console

--- Program.cs
+++ Program.cs
@@ -9,21 +9,24 @@ namespace task2
         static void Main(string[] args)
         {
             // Console.WriteLine("Enter the year: ");
-            int year = 2020;
+            int year = 2019;
             // Console.WriteLine("Enter the month(number): ");
-            int month = 2;
-            Drawcalendar(year, month);
+            int month = 7;
+            DrawCalendar(year, month);
+            DrawCalendar();
+            DrawCalendar(month);
             
         }
 
         static void CreateHeader(int year, int month)
         {
+            String[] months = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"}; 
             DateTime start = new DateTime(year, month, 1);
-            Console.WriteLine($"\t\t{year} {start}\t");
+            Console.WriteLine($"\t\t{year} {months[start.Month-1]}\t");
             Console.WriteLine("Mo\tTu\tWe\tTh\tFr\tSa\tSu");
         }
         
-        static void Drawcalendar(int year, int month)
+        static void DrawCalendar(int year, int month)
         {
             CreateHeader(year, month);
             int[,] dates = new int[6, 7];
@@ -33,7 +36,7 @@ namespace task2
             int firstDay = 1;
             int numberDay = (int)start.DayOfWeek;
             // Console.WriteLine(numberDay);
-            
+            DateTime localDate = DateTime.Now;
             for (int i = 0; i < dates.GetLength(0); i++)
             {
                 for (int j = 0; j < dates.GetLength(1) && firstDay - numberDay + 1 <= countDays; j++)
@@ -43,12 +46,35 @@ namespace task2
                     if (dates[i,j] < 1) {
                         Console.Write("\t");
                     } else if(dates[i,j] <= countDays) {
-                        Console.Write($"{dates[i, j]}\t");
+                        if(j < 5)
+                        {
+                            Console.Write($"{dates[i, j]}\t");
+                        } else if (dates[i,j] == localDate.Day) {
+                            Console.Write($"{dates[i, j]}\t", Color.Pink);
+                        }
+                        else {
+                            Console.Write($"{dates[i, j]}\t", Color.Red);
+                        }
+                        
                     }
 
                 }
                 Console.WriteLine();
             }
         }
+
+        static void DrawCalendar()
+            {
+                DateTime localDate = DateTime.Now;
+                // Console.WriteLine(localDate);
+                DrawCalendar(localDate.Year, localDate.Month);
+            }
+
+        static void DrawCalendar(int month)
+            {
+                DateTime localDate = DateTime.Now;
+                // Console.WriteLine(localDate);
+                DrawCalendar(localDate.Year, month);
+            }
     }
 }
